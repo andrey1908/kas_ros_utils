2e624adec26e6c0916400d28daac3afffd77a0f0
andrey1908 <andrey.krishtopik@mail.ru>
2021-04-14 19:14:51 +0300

    remove --target-frame argument

--- scripts/publish_odometry_transformation.py
+++ scripts/publish_odometry_transformation.py
@@ -11,19 +11,16 @@ import argparse
 def build_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--odom-topic', required=True, type=str)
-    parser.add_argument('--target-frame', required=True, type=str)
     return parser
 
 
 def odom_received(odom):
-    global target_frame
-
     global tfBroadcaster
     global tfBuffer
 
     odom_trans = geometry_msgs.msg.TransformStamped()
     odom_trans.header = odom.header
-    odom_trans.child_frame_id = target_frame
+    odom_trans.child_frame_id = odom.child_frame_id
     odom_trans.transform.translation = odom.pose.pose.position
     odom_trans.transform.rotation = odom.pose.pose.orientation
     tfBroadcaster.sendTransform(odom_trans)
@@ -33,7 +30,6 @@ if __name__ == '__main__':
     parser = build_parser()
     args, _ = parser.parse_known_args()
     odom_topic = args.odom_topic
-    target_frame = args.target_frame
     
     rospy.init_node('publish_odometry_transformation')
     tfBroadcaster = tf2_ros.TransformBroadcaster()
