df12180af3e2af729aa35c27c73b90d0e5098468
andrey1908 <andrey.krishtopik@mail.ru>
2021-10-09 19:52:08 +0300

    minor changes in read_transforms.py

--- scripts/read_transforms.py
+++ scripts/read_transforms.py
@@ -10,13 +10,13 @@ def build_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-from', '--from-frame', required=True, type=str)
     parser.add_argument('-to', '--to-frame', required=True, type=str)
-    parser.add_argument('-rate', '--rate', type=int, default=100)
     parser.add_argument('-out-bag', '--out-bag-file', required=True, type=str)
     parser.add_argument('-out-topic', '--out-topic-name', type=str, default='/transforms')
+    parser.add_argument('-rate', '--rate', type=int, default=100)
     return parser
 
 
-def read_transforms(from_frame, to_frame, rate, out_bag_file, out_topic_name):
+def read_transforms(from_frame, to_frame, out_bag_file, out_topic_name='/transforms', rate=100):
     r = rospy.Rate(rate)
     last_transform = TransformStamped()
     transforms_written = 0
@@ -24,14 +24,14 @@ def read_transforms(from_frame, to_frame, rate, out_bag_file, out_topic_name):
         print("Recording...")
         while not rospy.is_shutdown():
             try:
-                transform = tf_buffer.lookup_transform(from_frame, to_frame, rospy.Time())
+                transform = tf_buffer.lookup_transform(from_frame, to_frame, rospy.Time(0))
             except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
                 try:
                     r.sleep()
                 except (rospy.ROSInterruptException):
                     break
                 continue
-            if last_transform != transform:
+            if transform != last_transform:
                 bag.write(out_topic_name, transform, rospy.get_rostime())
                 transforms_written += 1
                 last_transform = transform
