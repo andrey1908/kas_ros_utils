7e1cc6794b8f0136d3b3ea42b190e2265cbead7a
andrey1908 <andrey.krishtopik@mail.ru>
2021-10-13 10:20:39 +0300

    add make_video_from_rosbag_images.py

---
+++ scripts/make_video_from_rosbag_images.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import argparse
+import rosbag
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+from tqdm import tqdm
+
+
+def build_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-rosbag', '--rosbag-file', required=True, type=str)
+    parser.add_argument('-t', '--topic', required=True, type=str)
+    parser.add_argument('-out', '--out-file', required=True, type=str)
+    parser.add_argument('-fourcc', '--fourcc', type=str, default='mp4v')
+    parser.add_argument('-fps', '--fps', type=int, default=30)
+    return parser
+
+
+def make_video_from_rosbag_images(rosbag_file, topic, out_file, fourcc='mp4v', fps=30):
+    bridge = CvBridge()
+    with rosbag.Bag(rosbag_file, 'r') as bag:
+        out = None
+        messages_number = bag.get_message_count(topic)
+        if messages_number == 0:
+            print('Topic {} not found'.format(topic))
+            return
+        messages_reader = bag.read_messages(topic)
+        for _, msg, t in tqdm(messages_reader, total=messages_number):
+            image = bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+            if out is None:
+                out = cv2.VideoWriter(out_file, cv2.VideoWriter_fourcc(*fourcc), fps, (image.shape[1], image.shape[0]))
+            out.write(image)
+        out.release()
+
+
+if __name__ == '__main__':
+    parser = build_parser()
+    args = parser.parse_args()
+    make_video_from_rosbag_images(**vars(args))
