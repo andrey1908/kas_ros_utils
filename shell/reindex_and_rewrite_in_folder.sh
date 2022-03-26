20538ee396edac73661466b2d343d51ada7189b2
andrey1908 <andrey.krishtopik@mail.ru>
2022-03-26 14:55:35 +0300

    add shell/reindex_and_rewrite_in_folder.sh for reindexing and rewriting bags in folder

---
+++ shell/reindex_and_rewrite_in_folder.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Specify folder with *.bag files to process"
+    exit 1
+fi
+
+if [ ! -d "$1" ]; then
+    echo "Folder $1 doesn't exist"
+    exit 1
+fi
+
+rosbags=$(find $1 -name '*.bag')
+echo $rosbags | tr ' ' '\n'
+echo ''
+
+rewrite_rosbag_script=$(dirname $0)/../scripts/rewrite_rosbag.py
+if [ ! -f "$rewrite_rosbag_script" ]; then
+    echo "Script for rewriting doesn't exist. Expected path: $rewrite_rosbag_script"
+    exit 1
+fi
+
+echo "Start processing..."
+echo ''
+
+for rosbag in $rosbags; do
+    (echo $rosbag && \
+    echo "Reindexing..."
+    orig_rosbag=${rosbag/%.bag/.orig.bag} && \
+    rosbag reindex $rosbag && \
+    rm $orig_rosbag && \
+    echo "Rewriting..."
+    rewritten_rosbag=${rosbag/%.bag/_rewritten.bag} && \
+    python $rewrite_rosbag_script -rosbag $rosbag -out $rewritten_rosbag && \
+    mv $rosbag $orig_rosbag && \
+    mv $rewritten_rosbag $rosbag && \
+    rm $orig_rosbag && \
+    echo "Done."
+    echo '') || exit 1
+done
+
+touch $1/all_reindexed_and_rewritten.txt
+
+echo "Success!"
+
